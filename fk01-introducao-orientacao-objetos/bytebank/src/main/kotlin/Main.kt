d4bb2b73fb86443ef4554a1011b1e533955d91ef
WybsonSantana <wybson@hotmail.com>
2022-06-29 23:18:20 -0300

    Aula 1: Criando variáveis

--- fk01-introducao-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
+++ fk01-introducao-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Bem-vindo(a) ao ByteBank!")
+    println("Bem-vindo(a) ao ByteBank!\n")
+
+    val titular: String = "Fulano de Tal"
+    val agencia: String = "0017"
+    val numeroConta: String = "1000"
+    var saldo: Double = 0.0
+
+    saldo = 150 + 2.49
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Agência: $agencia")
+    println("Número da conta: $numeroConta")
+    println("Saldo: R$ %.2f".format(saldo))
 }
