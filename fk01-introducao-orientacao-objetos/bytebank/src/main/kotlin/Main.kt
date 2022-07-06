51beb93943be8b58dd532d80f889483f6b06942b
WybsonSantana <wybson@hotmail.com>
2022-07-06 20:42:13 -0300

    Aula 2: Utilizando estrutura de repetição

--- fk01-introducao-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
+++ fk01-introducao-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
@@ -6,24 +6,45 @@ fun main() {
     val numeroConta: String = "1000"
     var saldo: Double = 0.0
 
-    saldo = 100 + 2.49
-    saldo -= 136.89
+    saldo = 100.0
+    saldo -= 250.0
 
     println("Titular: $titular")
-    println("Agência: $agencia")
-    println("Número da conta: $numeroConta")
+    println("Agência: $agencia | Número da conta: ${numeroConta.toInt()}")
+    println("Saldo da conta: R$ %.2f".format(saldo))
+    testaCondicaoDaConta(saldo)
 
+    for (i in 1..5) {
+        saldo += 10 * i
+        println("Titular: $titular $i")
+        println("Agência: $agencia | Número da conta: ${numeroConta.toInt() + i}")
+        println("Saldo da conta: R$ %.2f".format(saldo))
+        testaCondicaoDaConta(saldo)
+    }
+
+    var i = 6
+    while (i <= 10) {
+        saldo += 10 * i
+        println("Titular: $titular $i")
+        println("Agência: $agencia | Número da conta: ${numeroConta.toInt() + i}")
+        println("Saldo da conta: R$ %.2f".format(saldo))
+        testaCondicaoDaConta(saldo)
+        i++
+    }
+}
+
+fun testaCondicaoDaConta(saldo: Double) {
     when {
-        saldo > 0.0 -> println("Seu saldo está positivo!\nSaldo da conta: R$ %.2f".format(saldo))
-        saldo < 0.0 -> println("Seu saldo está negativo!\nSaldo da conta: R$ %.2f".format(saldo))
-        else -> println("Seu saldo está zerado!\nSaldo da conta: R$ %.2f".format(saldo))
+        saldo > 0.0 -> println("Seu saldo está positivo!")
+        saldo < 0.0 -> println("Seu saldo está negativo!")
+        else -> println("Seu saldo está zerado!")
     }
 
     if (saldo > 0.0) {
-        println("Querendo fazer aquela viagem com a família? A gente te ajuda!")
+        println("Querendo fazer aquela viagem com a família? A gente te ajuda!\n")
     } else if (saldo < 0.0) {
-        println("As contas apertaram este mês? A gente tem o crédito ideal para você!")
+        println("As contas apertaram este mês? A gente tem o crédito ideal para você!\n")
     } else {
-        println("Precisando de uma graninha extra? Fale com a gente!")
+        println("Precisando de uma graninha extra? Fale com a gente!\n")
     }
 }
