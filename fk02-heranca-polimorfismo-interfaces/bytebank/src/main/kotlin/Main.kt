f105d0e782895826a513a61c0af07ad38616fc53
WybsonSantana <wybson@hotmail.com>
2022-07-10 16:17:57 -0300

    Aula 5: Criando a classe cliente

--- fk02-heranca-polimorfismo-interfaces/bytebank/src/main/kotlin/Main.kt
+++ fk02-heranca-polimorfismo-interfaces/bytebank/src/main/kotlin/Main.kt
@@ -1,25 +1,4 @@
 fun main() {
-    val gerenteBeltrano = Gerente(
-        nome = "Beltrano da Silva",
-        cargo = "Gerente",
-        cpf = "930.133.220-55",
-        email = "beltranodasilva@bytebank.com.br",
-        salario = 10000.0,
-        senha = 2749
-    )
-
-    val diretorCiclano = Diretor(
-        nome = "Ciclano do Ramo",
-        cargo = "Diretor",
-        cpf = "380.157.620-57",
-        email = "ciclanodoramo@bytebank.com.br",
-        salario = 20000.0,
-        senha = 3264
-    )
-
-    val sistemaInterno = SistemaInterno()
-
-    sistemaInterno.logar(gerenteBeltrano, 2749)
-    sistemaInterno.logar(gerenteBeltrano, 1234)
+    testaAutenticacao()
 
 }
