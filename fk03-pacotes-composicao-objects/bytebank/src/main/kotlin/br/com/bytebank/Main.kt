c1da1b1e9cc2b55a4b09a27b7d559e3f6b74b664
WybsonSantana <wybson@hotmail.com>
2022-07-15 20:52:01 -0300

    Aula 4: Testando impressões

--- fk03-pacotes-composicao-objects/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk03-pacotes-composicao-objects/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -1,43 +1,13 @@
 package br.com.bytebank
 
-import br.com.bytebank.modelo.*
-import br.com.bytebank.teste.*
+import br.com.bytebank.modelo.Cliente
+import br.com.bytebank.modelo.Endereco
 
 fun main() {
-    testaContasDiferentes()
-
-    val clienteTentano = Cliente(
-        nome = "Tentano da Sorte",
-        cpf = "779.508.689-61",
-        email = "tentanodasorte@mail.com",
-        senha = 7777
-    )
-
-    val contaCorrenteTentano = ContaCorrente(
-        numeroConta = "2001",
-        titular = clienteTentano
-    )
-
-    val contaPoupancaTentano = ContaPoupanca(
-        numeroConta = "2002",
-        titular = clienteTentano
-    )
-
-    println("\nNúmero de contas criadas: ${Conta.Contador.total}\n")
-
-    val kotano = object : Autenticavel {
-        val nome: String = "Kotano Pano"
-        val cpf: String = "305.711.404-08"
-        val email: String = "kotanopano@mail.com.br"
-        val senha: Int = 1326
-
-        override fun autenticar(senha: Int): Boolean = this.senha == senha
-    }
-
-    val sistemaInterno = SistemaInterno()
-    sistemaInterno.logar(kotano, 1326)
-
-    println("Nome do cliente: ${kotano.nome}")
-    println("CPF: ${kotano.cpf}")
-    println("E-mail: ${kotano.email}")
+    println()
+    println(17)
+    println(1.6)
+    println(true)
+    println("Kotlin")
+    println('K')
 }
