0bd753954ccc92abf291932f4249eb8c3ed1ea56
WybsonSantana <wybson@hotmail.com>
2022-07-15 21:33:36 -0300

    Aula 4: Testando o casting

--- fk03-pacotes-composicao-objects/bytebank/src/main/kotlin/br/com/bytebank/modelo/CalculadoraDeBonificacao.kt
+++ fk03-pacotes-composicao-objects/bytebank/src/main/kotlin/br/com/bytebank/modelo/CalculadoraDeBonificacao.kt
@@ -4,7 +4,8 @@ class CalculadoraDeBonificacao {
     var total: Double = 0.0
         private set
 
-    fun registra(funcionario: Funcionario) {
+    fun registra(funcionario: Any) {
+        funcionario as Funcionario
         this.total += funcionario.bonificacao()
     }
 }
