cce7cac27b7a86b0f0833fa965d8e5236dbb28b0
WybsonSantana <wybson@hotmail.com>
2022-07-16 13:28:22 -0300

    Aula 1: Testando a pilha de execução

--- fk04-exceptions-null-safety/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk04-exceptions-null-safety/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -1,5 +1,21 @@
 package br.com.bytebank
 
 fun main() {
+    println("Início main()")
+    funcao1()
+    println("Fim main()")
+}
 
+fun funcao1() {
+    println("Início funcao1()")
+    funcao2()
+    println("Fim funcao1()")
+}
+
+fun funcao2() {
+    println("Início funcao2()")
+    for (i in 1..5) {
+        println(i)
+    }
+    println("Fim funcao2()")
 }
