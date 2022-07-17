471bbf7eae1555fc81ca151d1f185177693458e9
WybsonSantana <wybson@hotmail.com>
2022-07-17 17:59:44 -0300

    Aula 5: Testando o elvis operator e o safe cast

--- fk04-exceptions-null-safety/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk04-exceptions-null-safety/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -3,10 +3,25 @@ package br.com.bytebank
 import br.com.bytebank.modelo.Endereco
 
 fun main() {
-    var endereco: Endereco? = Endereco(logradouro = "Rua 1")
-    val logradouroNovo: String? = endereco?.logradouro
+    var endereco: Endereco? = Endereco(
+        logradouro = "Rua 1",
+        complemento = "Casa 2"
+    )
 
     endereco?.let {
         println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("O campo complemento não pode ser vazio.")
+
+        println(tamanhoComplemento)
     }
+
+    testaSafeCast(17)
+    testaSafeCast("")
+
+}
+
+fun testaSafeCast(valor: Any) {
+    val numero: Int? = valor as? Int
+    println(numero)
 }
