c32f48ffed754036314a6385d80bf185ed09ac95
WybsonSantana <wybson@hotmail.com>
2022-07-23 18:55:37 -0300

    Aula 3: Somatória com Reduce

--- fk05-collections-array-list/collections/src/main/kotlin/array/SalariosComBigDecimal.kt
+++ fk05-collections-array-list/collections/src/main/kotlin/array/SalariosComBigDecimal.kt
@@ -8,19 +8,32 @@ fun main() {
     println(salarios.contentToString())
 
     val reajuste = "0.1".toBigDecimal()
-    val salariosComReajuste: Array<BigDecimal> = salarios.map { salario ->
-        if (salario < "5000.0".toBigDecimal()) {
-            salario + "500".toBigDecimal()
-        } else {
-            salario.plus(salario.times(reajuste)).setScale(2, RoundingMode.UP)
-        }
-    }.toTypedArray()
+    val salariosComReajuste: Array<BigDecimal> = salarios
+        .map { salario -> calculaReajusteSalarial(salario, reajuste) }
+        .toTypedArray()
 
     println("Salários com reajuste: ${salariosComReajuste.contentToString()}")
+
+    val gastoInicial = salariosComReajuste.somatoria()
+    println("Gasto inicial: ${gastoInicial}")
 }
 
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     return Array<BigDecimal>(valores.size) { i ->
         valores[i].toBigDecimal()
+    }
+}
+
+fun calculaReajusteSalarial(salario: BigDecimal, reajuste: BigDecimal): BigDecimal {
+    return if (salario < "5000.0".toBigDecimal()) {
+        salario + "500".toBigDecimal()
+    } else {
+        salario.plus(salario.times(reajuste)).setScale(2, RoundingMode.UP)
+    }
+}
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
     }
 }
