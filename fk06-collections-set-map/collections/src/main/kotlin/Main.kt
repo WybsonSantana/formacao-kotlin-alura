aba95945327c2078ff056ffd11ed74ce2acb1d63
WybsonSantana <wybson@hotmail.com>
2022-07-25 07:41:13 -0300

    Aula 1: Introdução a Collections

--- fk06-collections-set-map/collections/src/main/kotlin/Main.kt
+++ fk06-collections-set-map/collections/src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
+    val nomes: List<String> = listOf("Fulano", "Beltrano", "Ciclano", "Mengano", "Zotano")
 
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println(nomes)
+    println("Contém o nome \"Fulano\"? ${nomes.contains("Fulano")}")
+    println("Tamanho da coleção: ${nomes.size}")
 }
