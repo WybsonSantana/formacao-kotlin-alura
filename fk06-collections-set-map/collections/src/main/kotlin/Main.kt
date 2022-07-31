edde8fa09e3e9c2a7d11b700f0571c6742caa4f3
WybsonSantana <wybson@hotmail.com>
2022-07-31 08:39:21 -0300

    Aula 1: Implementando o banco de nomes

--- fk06-collections-set-map/collections/src/main/kotlin/Main.kt
+++ fk06-collections-set-map/collections/src/main/kotlin/Main.kt
@@ -1,4 +1,24 @@
 fun main() {
+    val banco = BancoDeNomes()
+    // banco.nomes.add("Fulano")
+    banco.salvar("Fulano")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+    val nomes: Collection<String> get() = dados
+
+    fun salvar(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
     val nomes: List<String> = listOf("Fulano", "Beltrano", "Ciclano", "Mengano", "Zotano")
 
     for (nome in nomes) {
