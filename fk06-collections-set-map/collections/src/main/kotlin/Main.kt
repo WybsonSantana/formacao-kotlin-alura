4f1280f3d744ba838d0dd18e0f993a70c5fca628
WybsonSantana <wybson@hotmail.com>
2022-07-31 09:40:46 -0300

    Aula 2: Testando comportamentos com o Set

--- fk06-collections-set-map/collections/src/main/kotlin/Main.kt
+++ fk06-collections-set-map/collections/src/main/kotlin/Main.kt
@@ -2,6 +2,14 @@ fun main() {
     val assistiramCursoJava: Set<String> = setOf("Fulano", "Beltrano", "Ciclano")
     val assistiramCursoKotlin: Set<String> = setOf("Fulano", "Mengano", "Zotano")
 
+    println(assistiramCursoJava + assistiramCursoKotlin)
+    println(assistiramCursoJava.union(assistiramCursoKotlin))
+
+    println(assistiramCursoJava - assistiramCursoKotlin)
+    println(assistiramCursoJava.subtract(assistiramCursoKotlin))
+
+    println(assistiramCursoJava.intersect(assistiramCursoKotlin))
+
     val assistiramAmbosCursos1: Set<String> = assistiramCursoJava + assistiramCursoKotlin
     println(assistiramAmbosCursos1)
 
