7448dbfa79e58ebe37b220724f5b1ded210ba84f
WybsonSantana <wybson@hotmail.com>
2022-07-31 09:28:54 -0300

    Aula 2: Testando o set

--- fk06-collections-set-map/collections/src/main/kotlin/Main.kt
+++ fk06-collections-set-map/collections/src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
 fun main() {
-val assistiramCursoJava = listOf("Fulano", "Beltrano", "Ciclano")
-val assistiramCursoKotlin= listOf("Fulano", "Mengano", "Zotano")
-    val assistiramAmbosCursos = assistiramCursoJava + assistiramCursoKotlin
-    println(assistiramAmbosCursos.distinct())
+    val assistiramCursoJava: Set<String> = setOf("Fulano", "Beltrano", "Ciclano")
+    val assistiramCursoKotlin: Set<String> = setOf("Fulano", "Mengano", "Zotano")
+
+    val assistiramAmbosCursos1: Set<String> = assistiramCursoJava + assistiramCursoKotlin
+    println(assistiramAmbosCursos1)
+
+    val assistiramAmbosCursos2 = mutableSetOf<String>()
+    assistiramAmbosCursos2.addAll(assistiramCursoJava)
+    assistiramAmbosCursos2.addAll(assistiramCursoKotlin)
+    assistiramAmbosCursos2.add("Tentano")
+    assistiramAmbosCursos2.add("Pebano")
+    println(assistiramAmbosCursos2)
 }
