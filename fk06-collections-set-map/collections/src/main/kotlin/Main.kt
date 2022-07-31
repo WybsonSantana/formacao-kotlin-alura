cf84d1f64bda0c20ac8266e6d5cc3f3938e5db73
WybsonSantana <wybson@hotmail.com>
2022-07-31 14:45:14 -0300

    Aula 3: Testando comportamentos de escrita do Map

--- fk06-collections-set-map/collections/src/main/kotlin/Main.kt
+++ fk06-collections-set-map/collections/src/main/kotlin/Main.kt
@@ -1,13 +1,38 @@
 fun main() {
-    val pedidos = mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
     println(pedidos)
     val pedido = pedidos[3]
-    pedido.let {
+    pedido?.let {
         println("Valor do pedido: $it")
     }
 
-    for (pedido: Map.Entry<Int, Double> in pedidos) {
-        println("Número do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
+    for (item: Map.Entry<Int, Double> in pedidos) {
+        println("Número do pedido: ${item.key}")
+        println("Valor do pedido: ${item.value}")
     }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+
+    pedidos.put(5, 80.0)
+    println(pedidos)
+
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+
+    pedidos.remove(3, 50.0)
+    println(pedidos)
 }
