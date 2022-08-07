cc6abe4edc57849f8c23a8095f2d93005ce7b8a6
WybsonSantana <wybson@hotmail.com>
2022-08-07 09:43:53 -0300

    Aula 5: Testando o agrupamento

--- fk06-collections-set-map/collections/src/main/kotlin/Main.kt
+++ fk06-collections-set-map/collections/src/main/kotlin/Main.kt
@@ -3,7 +3,8 @@ fun main() {
         Pedido(1, 20.0),
         Pedido(2, 60.0),
         Pedido(3, 30.0),
-        Pedido(4, 70.0)
+        Pedido(4, 70.0),
+        Pedido(5, 40.0)
     )
 
     println(pedidos)
@@ -19,6 +20,36 @@ fun main() {
     }
 
     println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(numero = 2, valor = 60.0)])
+
+    val pedidosMaiorQue50: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+        pedido.valor > 50.0
+    }
+
+    println(pedidosMaiorQue50)
+
+    val pedidosFreteGratisAgrupados: Map<Boolean, List<Pedido>> = pedidos.groupBy { pedido ->
+        pedido.valor > 50.0
+    }
+
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[true])
+
+    val nomes = listOf(
+        "Fulano",
+        "Beltrano",
+        "Ciclano",
+        "Mengano",
+        "Pebano",
+        "Zotano"
+    )
+
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+
+    println(agenda)
+    println(agenda['F'])
 }
 
 data class Pedido(
