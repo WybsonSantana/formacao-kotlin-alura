64fd520f76bde471f08d107a0a129fedf449d036
WybsonSantana <wybson@hotmail.com>
2022-08-07 22:02:17 -0300

    Aula 1: Inicializando com literais de função

--- fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -1,9 +1,25 @@
 package br.com.bytebank
 
 fun main() {
+    val minhaFuncaoLambda = {
+        println("Executa como lambda")
+    }
+
+    println(minhaFuncaoLambda())
+
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como anônima")
+    }
+
+    println(minhaFuncaoAnonima())
+}
+
+fun testaTipoFuncaoReferencia() {
     val minhaFuncao = ::teste
     println(minhaFuncao)
+}
 
+fun testaTipoFuncaoClasse() {
     val minhaFuncaoClasse: () -> Unit = Teste()
     println(minhaFuncaoClasse())
 }
