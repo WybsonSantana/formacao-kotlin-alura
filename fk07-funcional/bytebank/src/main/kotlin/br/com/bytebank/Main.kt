f3bbfa8027ad40fc99622066d613669698f826e1
WybsonSantana <wybson@hotmail.com>
2022-08-16 21:46:33 -0300

    Aula 3: Implementando códigos com o let

--- fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -1,5 +1,28 @@
 package br.com.bytebank
 
-fun main() {
+import br.com.bytebank.modelo.Endereco
 
+fun main() {
+    val endereco1 = Endereco(
+        logradouro = "Rua sem Saída",
+        numero = "17"
+    )
+
+    val enderecoEmMaiusculo = "${endereco1.logradouro}, ${endereco1.numero}".uppercase()
+    println(enderecoEmMaiusculo)
+
+    val endereco2 = Endereco(
+        logradouro = "Rua sem Saída",
+        numero = "19"
+    ).let { endereco2 ->
+        "${endereco2.logradouro}, ${endereco2.numero}".uppercase()
+    }.let(::println)
+
+    listOf<Endereco>(
+        Endereco(complemento = "Casa"),
+        Endereco(complemento = "Apto."),
+        Endereco()
+    ).filter { endereco ->
+        endereco.complemento.isNotEmpty()
+    }.let(::println)
 }
