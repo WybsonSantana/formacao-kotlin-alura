b22ac93e6acafc4078499e337dde3e91650bb2c0
WybsonSantana <wybson@hotmail.com>
2022-08-20 12:13:39 -0300

    Aula 3: Implementando Higher Ordem Functions

--- fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -1,6 +1,8 @@
 package br.com.bytebank
 
+import br.com.bytebank.modelo.Autenticavel
 import br.com.bytebank.modelo.Endereco
+import br.com.bytebank.modelo.SistemaInterno
 
 fun main() {
     val endereco1 = Endereco(
@@ -22,7 +24,25 @@ fun main() {
         Endereco(complemento = "Casa"),
         Endereco(complemento = "Apto."),
         Endereco()
-    ).filter { endereco ->
+    ).filter(predicate = { endereco ->
         endereco.complemento.isNotEmpty()
-    }.let(::println)
+    }).let(block = (::println))
+
+    somar(11, 17, resultado = (::println))
+
+    val autenticavel = object : Autenticavel {
+        val senha = 1234
+
+        override fun autenticar(senha: Int) = this.senha == senha
+    }
+
+    SistemaInterno().logar(autenticavel, 1234, autenticado = {
+        println("Realizar pagamento")
+    })
+}
+
+fun somar(x: Int, y: Int, resultado: (Int) -> Unit) {
+    println("Antes de somar")
+    resultado(x + y)
+    println("Após somar")
 }
--- fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/modelo/SistemaInterno.kt
+++ fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/modelo/SistemaInterno.kt
@@ -2,9 +2,10 @@ package br.com.bytebank.modelo
 
 class SistemaInterno {
 
-    fun logar(usuario: Autenticavel, senha: Int) {
+    fun logar(usuario: Autenticavel, senha: Int, autenticado: () -> Unit = {}) {
         if (usuario.autenticar(senha)) {
             println("Seja bem-vindo(a) ao ByteBank!\n")
+            autenticado()
         } else {
             println("Falha na autenticação!\n")
         }
