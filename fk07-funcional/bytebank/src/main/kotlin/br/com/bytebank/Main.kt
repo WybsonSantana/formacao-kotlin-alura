51ef15a4e13d3b3bf5d4f52e6f0ec3aefef7b122
WybsonSantana <wybson@hotmail.com>
2022-08-20 16:31:29 -0300

    Aula 5: Calculando rendimento com run

--- fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
+++ fk07-funcional/bytebank/src/main/kotlin/br/com/bytebank/Main.kt
@@ -1,10 +1,43 @@
 package br.com.bytebank
 
+import br.com.bytebank.modelo.Cliente
+import br.com.bytebank.modelo.ContaPoupanca
 import br.com.bytebank.modelo.Endereco
 
 fun main() {
     testaWith()
+    testaRun()
+}
 
+fun testaRun() {
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("Taxa mensal: $taxaMensal")
+    val contaPoupanca = ContaPoupanca(
+        Cliente(
+            nome = "Fulano de Tal",
+            email = "fulanodetal@mail.com",
+            cpf = "123.456.890-09",
+            senha = 1234
+        ),
+        numeroConta = "1001"
+    )
+
+    contaPoupanca.run {
+        depositar(1000.0)
+        saldo * taxaMensal
+    }.let { rendimentoMensal ->
+        println("Rendimento mensal: $rendimentoMensal")
+    }
+
+    val rendimentoAnual = run {
+        var saldo = contaPoupanca.saldo
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("Simulação rendimento anual: $rendimentoAnual")
 }
 
 fun testaWith() {
